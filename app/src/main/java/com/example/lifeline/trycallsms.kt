74c22f8aac74e55e620c475d382c15eb49dbd7d5
aneesho999 <aneeshoak@gmail.com>
2023-10-02 06:22:10 +0530

    places calls

--- app/src/main/java/com/example/lifeline/trycallsms.kt
+++ app/src/main/java/com/example/lifeline/trycallsms.kt
@@ -1,17 +1,114 @@
 package com.example.lifeline
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.lifeline.ui.theme.LifeLineTheme
 
 class trycallsms : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LifeLineTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Button(
+                        colors = ButtonDefaults.outlinedButtonColors(Color.Black),
+                        onClick = {
+                            // backend: activate if needed
+                            val goBack = Intent(this@trycallsms, MainActivity::class.java)
+                            startActivity(goBack)
+                            finish()
+                        },
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text("X",color= Color.Yellow)
+                    }
+                }
+                Column(
+                    modifier= Modifier
+                        .padding(10.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                   Button(
+                       onClick = {
+                           checkCallPermissions()
+                            placeCall("+917057603762")
+                   }) {
+                       Text("Family")
+                   }
+                    Button(onClick = {
 
+                    }) {
+                        Text("Police")
+                    }
+                    Button(
+                        onClick = {
+
+                        }
+                    ) {
+                        Text("Ambulance")
+                    }
+                }
             }
         }
     }
+
+    private fun showMessage(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(true)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.show()
+    }
+
+    private fun placeCall(phoneNumber: String) {
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:$phoneNumber")
+
+        try {
+            startActivity(callIntent)
+        } catch (e: SecurityException) {
+            showMessage("error", "couldn't place call")
+        }
+    }
+
+    private fun checkCallPermissions(): Boolean {
+        val permissions = arrayOf(
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.CALL_PHONE
+        )
+
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, 1)
+                return false
+            }
+        }
+        return true
+    }
+
 }
